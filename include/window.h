bd4e6ad4d706798535ceb75cd55e9842eccdea34
Luan Thums <luancthums@gmail.com>
2026-04-01 15:22:08 -0300

    added C3D_Window type -- less manual work required for the user

--- include/window.h
+++ include/window.h
@@ -4,9 +4,25 @@
 #include "SDL2/SDL.h"
 #include <stdbool.h>
 
-SDL_Window *window_create(int width, int height, const char *title);
-SDL_Window *window_create_fullsize(const char *title);
-void window_grabCursor(SDL_Window *window, bool grab);
-void window_fullscreen(SDL_Window *window, bool flag);
+struct C3D_Window {
+	SDL_Window *window;
+	SDL_GLContext glContext;
+	int width;
+	int height;
+};
+
+typedef struct C3D_Window C3D_Window;
+
+C3D_Window *window_create(int width, int height, const char *title);
+C3D_Window *window_create_fullsize(const char *title);
+void window_grab_cursor(C3D_Window *window, bool grab);
+void window_fullscreen(C3D_Window *window, bool flag);
+
+/**
+ * @brief Process window events and return false if the window should be closed.
+ * @param window The window to process events for.
+ * @return true if the window should remain open, false if it should be closed.
+ */
+bool window_process(C3D_Window *window);
 
 #endif
